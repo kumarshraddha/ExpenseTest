b2a09be1858a612214f5e431625eb0bf1f4751ca
shraddhakumar <kumarshraddha98@gmail.com>
2026-03-18 19:10:07 +0530

    -removed TrendScreen.kt screenshot tests -removed kover -modified screenshottests for DashboardScreen.kt

--- feature-expenses/build.gradle.kts
+++ feature-expenses/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.compose.compiler)
-    id("org.jetbrains.kotlinx.kover")
     alias(libs.plugins.screenshot)
 }
 
@@ -71,10 +70,4 @@ dependencies {
     screenshotTestImplementation(libs.screenshot.validation.api)
     screenshotTestImplementation(libs.androidx.compose.ui.tooling)
     screenshotTestImplementation(kotlin("stdlib"))
-}
-
-kover {
-    reports {
-        variant("debug") { }
-    }
 }
